06aec3c0111eb5cab3fe79c7630f8fa465c32f6b
DrPleasure <114397562+DrPleasure@users.noreply.github.com>
2023-01-13 16:35:00 +0100

    d

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/Footer.jsx";
 import Movies from "./components/Movies";
 
 console.log("Link to hosted back-end with get all media query: https://gold-healthy-pangolin.cyclic.app/medias")
+console.log("Link to DOCUMENTATION: https://documenter.getpostman.com/view/24352757/2s8ZDSbQh9")
 
 
 function App() {
