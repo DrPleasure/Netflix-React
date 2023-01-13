6ef21abaddfbdba44bb4449d608203c9c148c10a
DrPleasure <114397562+DrPleasure@users.noreply.github.com>
2023-01-13 15:56:42 +0100

    d

--- src/components/FetchMedias.jsx
+++ src/components/FetchMedias.jsx
@@ -1,13 +1,15 @@
 
 const fetchMedias = async () => {
   try {
-    const response = await fetch('http://localhost:3001/medias');
+    const response = await fetch('https://gold-healthy-pangolin.cyclic.app/medias');
     const data = await response.json();
     return data;
-    console.log(data)
+   
   } catch (error) {
     console.error(error);
   }
 }
+
+console.log("Link to hosted back-end with get all media query: https://gold-healthy-pangolin.cyclic.app/medias")
 
 export default fetchMedias;
